69dba5edfcf0cb63c885da3664766badd98223bc
Tom Ware <tomjware@gmail.com>
2023-10-12 21:06:56 -0400

    Added Support for game backgrounds

--- scripts/build.js
+++ scripts/build.js
@@ -5,6 +5,7 @@ document.getElementById('buildText').innerHTML = versionTag;
 document.getElementById('changelog').innerHTML = `<h3>Changes to ` + versionTag + `</h3>
 <ul>
 <li>Changed Chat layout to be easier to track updates</li>
+<li>Added Support for Game Backgrounds (Add "background": "URL TO IMAGE" to the game.json file in the .apworld) [Currently only working with external images]</li>
 </ul>
 <h4>Upcoming Changes</h4>
 <ul>
