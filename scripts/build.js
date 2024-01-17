f94278a4ed9b781c659b65014b435f675c2cbd41
Tom Ware <tomjware@gmail.com>
2024-01-16 19:52:04 -0500

    Updated Changelog

--- scripts/build.js
+++ scripts/build.js
@@ -8,11 +8,13 @@ document.getElementById('changelog').innerHTML = `<h4>Please note: Manual Archip
 <li>Rework of backend to accomodate to the .APMANUAL files now provided with all Manual APs.</li>
 <li>Removed Deathlink tag when logging in. (Will probably bring this back once I look into Deathlink more.)</li>
 <li>Change the build to show 0.4.4 when connecting.</li>
-<li>Added Tag "ManualWeb" to show that it's the web client connecting.
+<li>Added Tag "ManualWeb" to show that it's the web client connecting.</li>
+<li>Added Color Coding for items and player names. (ALPHA VERSION) [Issues may exist.]</li>
 </ul>
 <h4>Known Issues</h4>
 <ul>
-<li>If two people are running the same Manual but different players, the tracker might "remove" checks from one of the players. The checks are not marked and you will need to use the Python tracker found in the Discord or server commands to release the check.</li>
+<li>If two people are running the same Manual but different players, the tracker might "remove" checks from one of the players. The checks are not marked and you will need to use the Python tracker found in the Discord or server commands to release the check. (Issue is possibly fixed, but more testing is needed.</li>
+<li>Items changing color on click not functioning. Currently unsure as to why this is happening, but should be fixed in the next update.</li>
 </ul>`
 
 // Get the modal
