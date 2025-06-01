cdfbd28db3cc9fc04d7b52f07728cc0d8a45f66f
Tom Ware <tomjware@gmail.com>
2025-06-01 14:06:37 -0400

    Fixed issue where tracker would work anymore

--- scripts/build.js
+++ scripts/build.js
@@ -6,6 +6,7 @@ document.getElementById('changelog').innerHTML = `<h4>Please note: Manual Archip
 <h3>Changes to ` + versionTag + `</h3>
 <ul>
 <li>Added update for v0.6.1 of Archipelago</li>
+<li>HOTFIX: Fixed issue where tracker would break upon joining server.</li>
 </ul>
 <h4>Known Issues</h4>
 <ul>
